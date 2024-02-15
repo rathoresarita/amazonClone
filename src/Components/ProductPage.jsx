991e72be0f948ef82c8fed9dbc1eb93c3325fb2e
rathoresarita <saritasinghrathore140@gmail.com>
2024-02-15 15:55:12 +0530

    added

--- src/Components/ProductPage.jsx
+++ src/Components/ProductPage.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { callApi } from '../utils/CallApi'
+
 
 
 const ProductPage = () => {
     const { id } = useParams()
+    const [product,setProduct]=useState(null)
+    const getProduct = () => {  
+        callApi(`data/products.json`).then((productResult) =>{
+            setProduct(productResult[id])
+            console.log('Product',product)
+        }
 
+
+    )}
+    useEffect(()=>{
+getProduct()
+    },[])
+
+if(!product?.title)
+{
+    return <h1>Loading Product ....</h1>
+}
     return (
-        <div>
-            Product  {id}
+        product &&
+        <div className='h-screen bg-amazonclone-background'>
+            <div className='min-w-[1500px] m-auto bg-orange-400'></div>
+            <div className='grid  grid-cols-10'>
+                {/* left */}
+                <div>
+
+                </div>
+                 {/* mid */}
+                 <div>
+
+                 </div>
+                  {/* right*/}
+            </div>
+            Product  {product.title}
         </div>
     )
 }
--- src/utils/CallApi.js
+++ src/utils/CallApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { BASE_URL } from '../Constants'
+import { BASE_URL } from '../utils/Constants'
 const config = {
     headers: {
         "Conten-Type": "application/json",
